53c26fcf23ae7dd625faba73194fb6136cb2edaa
root <root@ip-172-31-89-80.ec2.internal>
2020-05-22 18:10:00 +0000

    demo files

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "private-subnets" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.PRIVATE_SUBNET[count.index]
   map_public_ip_on_launch = "false"
-  availability_zone       = count.index > length(data.aws_availability_zones.available.names) ? data.aws_availability_zones.available.names[count.index] : null
+  availability_zone       = count.index > length(local.az_length) ? local.az_length[count.index] : null
 
   tags = {
     Name = "${terraform.workspace}-${var.NAME}-private-subnet-${count.index}"
